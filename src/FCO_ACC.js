9da22e400f17ca8c084ebb4b243acfb6e8ff6924
floriancourgey <florian@courgey.fr>
2020-02-03 16:00:40 +0100

    + refacto (lib vs project)

--- src/FCO_ACC.js
+++ src/FCO_ACC.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const soap = require('soap');
+const cheerio = require('cheerio'); // light jquery
+const moment = require('moment');
+const fs = require('fs-extra'); // filesystem extensions
 const xtkSessionWsdl = require.resolve(process.env.WSDL_XTK_SESSION);
+const xtkQueryDefWsdl = require.resolve(process.env.WSDL_XTK_QUERYDEF);
+const xtkSpecfileWsdl = require.resolve(process.env.WSDL_XTK_SPECFILE);
 
 if(!process.env.ACC_USER || !process.env.ACC_PWD || !process.env.ACC_ENDPOINT){
   console.log('Define .env.ACC_USER .env.ACC_PWD .env.ACC_ENDPOINT');
@@ -18,12 +23,16 @@ var logonArgs = {
 exports.mainArgs = {
   endpoint: process.env.ACC_ENDPOINT,
 };
-// exports.securityToken = securityToken;
-// exports.sessionToken = sessionToken;
+
+// XML Parsing Definition
+exports.htmlparserOptions = {
+  xmlMode: true,
+  lowerCaseTags: true,
+};
 
 // Logon
 console.log('Logging in');
-exports.Logon = function(onSuccess){
+exports.logon = function(onSuccess){
   soap.createClient(xtkSessionWsdl, exports.mainArgs, function(err, client) {
     client.Logon(logonArgs, function(err, result, rawResult) {
       if(err){
@@ -39,6 +48,90 @@ exports.Logon = function(onSuccess){
       console.log('securityToken:', exports.securityToken);
       console.log('sessionToken:', exports.sessionToken);
       onSuccess();
+    });
+  });
+}
+
+/**
+ * @param where string placed in <where>{where}</where>
+ */
+exports.getSpecFile = function(where, onSuccessHandler){
+  var args = {
+    sessiontoken : '',
+    entity : {$xml :
+      '<queryDef fullLoad="true" operation="get" schema="xtk:specFile">'+
+        '<select>'+
+          '<node expr="@namespace" hidden="true" />'+
+          '<node expr="@name" hidden="false" />'+
+          '<node expr="@label" />'+
+          '<node expr="[definition/@id]" />'+
+          '<node expr="[definition/@schema]" />'+
+          '<node expr="[definition/@automaticDefinition]" />'+
+          '<node expr="[definition/@lineCountMax]" />'+
+          '<node anyType="true" expr="[definition/where]" noComputeString="true" />'+
+          '<node anyType="true" expr="[definition/exclusions]" noComputeString="true" />'+
+          '<node anyType="true" expr="[definition/orderBy]" noComputeString="true" />'+
+        '</select>'+
+        '<where>'+
+          where+
+        '</where>'+
+      '</queryDef>'
+    },
+  }
+
+  soap.createClient(xtkQueryDefWsdl, exports.mainArgs, function(err, xtkQueryDefClient){
+    console.log('SOAP xtkQueryDefClient OK');
+    xtkQueryDefClient.addHttpHeader('X-Security-Token', exports.securityToken);
+    xtkQueryDefClient.addHttpHeader('cookie',  "__sessiontoken=" + exports.sessionToken);
+
+    xtkQueryDefClient.ExecuteQuery(args, function(err, result, rawResponse, soapHeader, rawRequest) {
+      console.log('SOAP ExecuteQuery OK');
+      if(err){
+        console.log('rawRequest', rawRequest);
+        console.log('soapHeader', soapHeader);
+        throw err;
+      }
+
+      onSuccessHandler(result, rawResponse, soapHeader, rawRequest);
+    });
+  });
+}
+
+exports.generateDoc = function(where, onSuccessHandler){
+  exports.getSpecFile(where, function(result, rawResponse, soapHeader, rawRequest){
+
+    const $ = cheerio.load(rawResponse, exports.htmlparserOptions);
+    console.log('XML Definition OK');
+    var definition = $('pdomOutput').html();
+
+    soap.createClient(xtkSpecfileWsdl, exports.mainArgs, function(err, xtkSpecfileClient){
+      console.log('SOAP xtkSpecfileClient OK');
+      xtkSpecfileClient.addHttpHeader('X-Security-Token', exports.securityToken);
+      xtkSpecfileClient.addHttpHeader('cookie',  "__sessiontoken=" + exports.sessionToken);
+
+      var args = {
+        sessiontoken: '',
+        entity: {$xml: definition},
+      };
+      xtkSpecfileClient.GenerateDoc(args, function(err, result, rawResponse, soapHeader, rawRequest){
+        console.log('SOAP GenerateDoc OK');
+        // save request to archives
+        const archiveRequest = 'archives/'+moment().format('YYYY/MM/DD/HHmmss-SSS')+'-request.xml';
+        fs.outputFileSync(archiveRequest, rawResponse, function (err) {
+          throw err;
+        });
+        if(err){
+          console.log('rawRequest', rawRequest);
+          throw err;
+        }
+        // save response to archives
+        const archiveResponse = 'archives/'+moment().format('YYYY/MM/DD/HHmmss-SSS')+'-response.xml';
+        fs.outputFileSync(archiveResponse, rawResponse, function (err) {
+          throw err;
+        });
+
+        onSuccessHandler(result, rawResponse, soapHeader, rawRequest);
+      });
     });
   });
 }
--- src/download.js
+++ src/download.js
@@ -4,13 +4,8 @@ const cheerio = require('cheerio'); // light jquery
 const fs = require('fs-extra'); // filesystem extensions
 const _ = require('lodash'); // js extensions
 const sanitize_filename = require("sanitize-filename"); // get clean filename
-const xtkQueryDefWsdl = require.resolve(process.env.WSDL_XTK_QUERYDEF);
-const xtkSpecfileWsdl = require.resolve(process.env.WSDL_XTK_SPECFILE);
-const moment = require('moment');
 const pd = require('pretty-data').pd;
 const instanceDir = process.env.INSTANCE_DIR;
-
-FCO_ACC.Logon(onLogonSuccess);
 
 if(!process.env.PACKAGES){
   console.log('Define .env.PACKAGES');
@@ -18,162 +13,101 @@ if(!process.env.PACKAGES){
 }
 
 const folders = [];
-// XML Parsing Definition
-const htmlparserOptions = {
-  xmlMode: true,
-  lowerCaseTags: true,
-};
 
-// Get Package Definition
-function onLogonSuccess(){
-  var args = {
-    sessiontoken : '',
-    entity : {$xml :
-      '<queryDef fullLoad="true" operation="get" schema="xtk:specFile">'+
-        '<select>'+
-          '<node expr="@namespace" hidden="true" />'+
-          '<node expr="@name" hidden="false" />'+
-          '<node expr="@label" />'+
-          '<node expr="[definition/@id]" />'+
-          '<node expr="[definition/@schema]" />'+
-          '<node expr="[definition/@automaticDefinition]" />'+
-          '<node expr="[definition/@lineCountMax]" />'+
-          '<node anyType="true" expr="[definition/where]" noComputeString="true" />'+
-          '<node anyType="true" expr="[definition/exclusions]" noComputeString="true" />'+
-          '<node anyType="true" expr="[definition/orderBy]" noComputeString="true" />'+
-        '</select>'+
-        '<where>'+
-          process.env.PACKAGES+
-        '</where>'+
-      '</queryDef>'
-    },
-  }
+function parseFinalPackage(result, rawResponse, soapHeader, rawRequest){
 
-  soap.createClient(xtkQueryDefWsdl, FCO_ACC.mainArgs, function(err, xtkQueryDefClient){
-    console.log('SOAP xtkQueryDefClient OK');
-    xtkQueryDefClient.addHttpHeader('X-Security-Token', FCO_ACC.securityToken);
-    xtkQueryDefClient.addHttpHeader('cookie',  "__sessiontoken=" + FCO_ACC.sessionToken);
+  const $ = cheerio.load(rawResponse, FCO_ACC.htmlparserOptions);
+  $('entities').each(function(i, elem){
+    const $this = $(this);
+    var namespacedSchema = $this.attr('schema');
+    var namespace = namespacedSchema.split(':')[0];
+    var schema = namespacedSchema.split(':')[1];
+    console.log('- Namespaced Schema: '+namespacedSchema);
+    $this.find(schema).each(function(i, elem){
+      const $this = $(this);
+      var dir, filename;
 
-    xtkQueryDefClient.ExecuteQuery(args, function(err, result, rawResponse, soapHeader, rawRequest) {
-      console.log('SOAP ExecuteQuery OK');
-      if(err){
-        console.log('rawRequest', rawRequest);
-        throw err;
+      if($this.children('folder').length){
+        var folderName = $this.first('folder').attr('name');
+
+        if(folderName !== undefined && folders[folderName] === undefined){
+          console.log('has new folder:', folderName);
+          // get folder full path via sql or soap
+          var args = {
+            sessiontoken : '',
+            entity : {$xml :
+              '<queryDef operation="get" schema="xtk:folder">'+
+              '<select><node expr="@fullName"/></select>'+
+              '<where><condition expr="@name = \'xtkJavascript\'"/></where>'+
+              '</queryDef>'
+            }
+          };
+          folders[folderName]= {};
+        }
+      } else {
+        /*
+        console.log('NO folder');
+        // get folder
+        console.log('before');
+        var camelCaseNamespacedSchema = namespace + schema[0].toUpperCase() + schema.substr(1);
+        console.log('camelCaseNamespacedSchema:', camelCaseNamespacedSchema);
+        getFolderFullNameByName(xtkQueryDefClient, camelCaseNamespacedSchema);
+        console.log('after');
+        */
       }
+      return;
 
-      const $ = cheerio.load(rawResponse, htmlparserOptions);
-      console.log('XML Definition OK');
-      var definition = $('pdomOutput').html();
-
-      // Get Package Result
-      soap.createClient(xtkSpecfileWsdl, FCO_ACC.mainArgs, function(err, xtkSpecfileClient){
-        console.log('SOAP xtkSpecfileClient OK');
-        xtkSpecfileClient.addHttpHeader('X-Security-Token', FCO_ACC.securityToken);
-        xtkSpecfileClient.addHttpHeader('cookie',  "__sessiontoken=" + FCO_ACC.sessionToken);
-
-        var args = {
-          sessiontoken: '',
-          entity: {$xml: definition},
-        };
-        xtkSpecfileClient.GenerateDoc(args, function(err, result, rawResponse, soapHeader, rawRequest){
-          console.log('SOAP GenerateDoc OK');
-          // save request to archives
-          const archiveRequest = 'archives/'+moment().format('YYYY/MM/DD/HHmmss-SSS')+'-request.xml';
-          fs.outputFileSync(archiveRequest, rawResponse, function (err) {
-            throw err;
-          });
-          if(err){
-            console.log('rawRequest', rawRequest);
-            throw err;
-          }
-          // save response to archives
-          const archiveResponse = 'archives/'+moment().format('YYYY/MM/DD/HHmmss-SSS')+'-response.xml';
-          fs.outputFileSync(archiveResponse, rawResponse, function (err) {
-            throw err;
-          });
-          // parse response
-          const $ = cheerio.load(rawResponse, htmlparserOptions);
-          $('entities').each(function(i, elem){
-            const $this = $(this);
-            var namespacedSchema = $this.attr('schema');
-            var namespace = namespacedSchema.split(':')[0];
-            var schema = namespacedSchema.split(':')[1];
-            console.log('- Namespaced Schema: '+namespacedSchema);
-            $this.find(schema).each(function(i, elem){
-              const $this = $(this);
-              var dir, filename;
-/*
-              if($this.children('folder').length){
-                console.log('has folder');
-                // get folder full path via sql or soap
-              } else {
-                console.log('NO folder');
-                // get folder
-                console.log('before');
-                var camelCaseNamespacedSchema = namespace + schema[0].toUpperCase() + schema.substr(1);
-                console.log('camelCaseNamespacedSchema:', camelCaseNamespacedSchema);
-                getFolderFullNameByName(xtkQueryDefClient, camelCaseNamespacedSchema);
-                console.log('after');
-              }
-              return;
-*/
-              // can be factorized but keep it this way ATM
-              // @todo get folder path from instance for schemas with @folder-id field
-              switch(namespacedSchema){
-                case 'xtk:srcSchema':
-                  dir = instanceDir+'/Administration/Configuration/Data schemas/'+$this.attr('namespace')+'/';
-                  filename = $this.attr('name')+'.html';
-                  break;
-                case 'xtk:form':
-                  dir = instanceDir+'/Administration/Configuration/Input forms/'+$this.attr('namespace')+'/';
-                  filename = $this.attr('name')+'.html';
-                  break;
-                case 'xtk:navtree':
-                  dir = instanceDir+'/Administration/Configuration/Navigation hierarchies/'+$this.attr('namespace')+'/';
-                  filename = $this.attr('name')+'.html';
-                  break;
-                case 'xtk:javascript':
-                  dir = instanceDir+'/Administration/Configuration/JavaScript codes/'+$this.attr('namespace')+'/';
-                  filename = $this.attr('name')+'.js';
-                  break;
-                case 'xtk:jssp':
-                  dir = instanceDir+'/Administration/Configuration/Dynamic JavaScript pages/'+$this.attr('namespace')+'/';
-                  filename = $this.attr('name')+'.js';
-                  break;
-                case 'xtk:formRendering':
-                  dir = instanceDir+'/Administration/Configuration/Form rendering/';
-                  filename = $this.attr('internalName')+'.css';
-                  break;
-                case 'xtk:sql':
-                  dir = instanceDir+'/Administration/Configuration/SQL scripts/'+$this.attr('namespace')+'/';
-                  filename = $this.attr('name')+'.sql';
-                  break;
-                case 'xtk:xslt':
-                  dir = instanceDir+'/Administration/Configuration/XSL style sheets/'+$this.attr('namespace')+'/';
-                  filename = $this.attr('name')+'.html';
-                  break;
-                case 'nms:typologyRule':
-                  dir = instanceDir+'/Administration/Configuration/Typology rules/';
-                  filename = $this.attr('name')+'.html';
-                  break;
-                case 'nms:typology':
-                  dir = instanceDir+'/Administration/Configuration/Typologies/';
-                  filename = $this.attr('name')+'.html';
-                  break;
-                default:
-                  console.log('Not yet implemented');
-                  return;
-              }
-              var path = dir+sanitize_filename(filename);
-              console.log('"'+$this.attr('name')+'" saved as "'+filename+'"');
-              // save
-              fs.outputFileSync(path, $.html($this), function (err) {
-                throw err;
-              });
-            });
-          });
-        });
+      // can be factorized but keep it this way ATM
+      // @todo get folder path from instance for schemas with @folder-id field
+      switch(namespacedSchema){/*
+        case 'xtk:srcSchema':
+          dir = instanceDir+'/Administration/Configuration/Data schemas/'+$this.attr('namespace')+'/';
+          filename = $this.attr('name')+'.html';
+          break;
+        case 'xtk:form':
+          dir = instanceDir+'/Administration/Configuration/Input forms/'+$this.attr('namespace')+'/';
+          filename = $this.attr('name')+'.html';
+          break;
+        case 'xtk:navtree':
+          dir = instanceDir+'/Administration/Configuration/Navigation hierarchies/'+$this.attr('namespace')+'/';
+          filename = $this.attr('name')+'.html';
+          break;
+        case 'xtk:javascript':
+          dir = instanceDir+'/Administration/Configuration/JavaScript codes/'+$this.attr('namespace')+'/';
+          filename = $this.attr('name')+'.js';
+          break;
+        case 'xtk:jssp':
+          dir = instanceDir+'/Administration/Configuration/Dynamic JavaScript pages/'+$this.attr('namespace')+'/';
+          filename = $this.attr('name')+'.js';
+          break;
+        case 'xtk:formRendering':
+          dir = instanceDir+'/Administration/Configuration/Form rendering/';
+          filename = $this.attr('internalName')+'.css';
+          break;
+        case 'xtk:sql':
+          dir = instanceDir+'/Administration/Configuration/SQL scripts/'+$this.attr('namespace')+'/';
+          filename = $this.attr('name')+'.sql';
+          break;
+        case 'xtk:xslt':
+          dir = instanceDir+'/Administration/Configuration/XSL style sheets/'+$this.attr('namespace')+'/';
+          filename = $this.attr('name')+'.html';
+          break;*/
+        case 'nms:typologyRule':
+          dir = instanceDir+'/Administration/Configuration/Typology rules/';
+          filename = $this.attr('name')+'.html';
+          break;
+        default:
+          console.log('Not yet implemented');
+          return;
+      }
+      var path = dir+sanitize_filename(filename);
+      console.log('"'+$this.attr('name')+'" saved as "'+filename+'"');
+      // save
+      fs.outputFileSync(path, $.html($this), function (err) {
+        throw err;
       });
+    });
+  });
 
 
       // for each workflow
@@ -205,8 +139,6 @@ function onLogonSuccess(){
         });
       });
       */
-    });
-  });
 }
 
 // can be factorized as
@@ -228,4 +160,9 @@ async function getFolderFullNameByName(xtkQueryDefClient, folderName){
     console.log('getFolderFullNameByName:', folderName, $('folder').attr('fullName'), '2');
   });
   console.log('getFolderFullNameByName:', folderName, '3');
+}
+
+FCO_ACC.logon(onLogonSuccess);
+function onLogonSuccess(){
+  FCO_ACC.generateDoc(process.env.PACKAGES, parseFinalPackage);
 }
